64cd1c13d8fd21b99192bf2e1c474481b2a25ef5
caozhe <2360505266@qq.com>
2019-11-19 17:11:37 +0800

    hhj

---
+++ tect02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, sum, term;
+    sum = 0;
+    printf("Enter n:\n");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {                                                                                                               
+        term = n % 10;
+        sum += term;
+        n = n / 10;
+    }
+
+    printf("sum is %d", sum);
+
+    return 0;
+}
