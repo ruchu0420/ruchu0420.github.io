e0798ecb896b6ca4ce230846c3ec87d160a75d5d
caozhe <2360505266@qq.com>
2019-12-02 08:34:37 +0800

    czz

--- test04.c
+++ test04.c
@@ -3,12 +3,20 @@ int main(void)
 
 {
     int cock, hen, chicken;
+
     for (cock = 0; cock <= 20; cock++)
+    {
         for (hen = 0; hen <= 33; hen++)
+
+        {
             for (chicken = 0; chicken <= 100; chicken++)
             {
                 if ((cock + hen + chicken == 100) && (5 * cock + 3 * hen + chicken / 3.0 == 100))
+                {
                     printf("cock=%3d,hen=%3d,chicken=%3d\n", cock, hen, chicken);
+                }
             }
+        }
+    }
     return 0;
 }
