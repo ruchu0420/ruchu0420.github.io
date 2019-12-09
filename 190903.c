cf3dc2a0e800c1184f1bbb0f469023f6e27c54d3
caozhe <2360505266@qq.com>
2019-12-09 09:26:38 +0800

    nianfenrunnian

---
+++ 190903.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int isLeapyear(int x)
+{
+
+    if ((x % 4 == 0) && (x % 100 != 0) || (x % 400 == 0))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main(void)
+{
+    int i, flag, count = 0;
+    for (i = 2000; i <= 2050; i++)
+    {
+        flag = isLeapyear(i);
+        if (flag == 1)
+        {
+            printf("%5d", i);
+            count++;
+        }
+    }
+    printf("%5d", count);
+    return 0;
+}
